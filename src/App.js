b90e30bc59d1798d22116342d454917a0eaa37b2
Stephanie White <stephanieiwhite11@gmail.com>
2020-09-04 17:46:20 -0400

    the action

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Blurb from "./Blurb";
+import Search from "./Search";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main-App">
+      <div class="container">
+        <h1>Let's Start Investing!</h1>
+
+        <div class="row">
+          <div class="col">
+            <Blurb />
+          </div>
+          <div class="col">
+            <Search />
+          </div>
+        </div>
+
+        <br />
+        <div>
+          <footer className="me"> Open Source code by Stephanie White </footer>
+          <br />
+        </div>
+      </div>
     </div>
   );
 }
